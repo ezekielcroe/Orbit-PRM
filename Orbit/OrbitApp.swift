0f1cd0e7dff9bc8dc301583fd8425c7520be0f78
Yeo Zhi Zheng <zhizhengy@hcahospicecare.org.sg>
2026-02-19 08:50:43 +0800

    Initial Commit

---
+++ Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by Zhi Zheng Yeo on 19/2/26.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
